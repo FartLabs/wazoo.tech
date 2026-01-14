54318527b7add76aeefd85a0666789c364aa320a
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2026-01-14 08:45:17 -0800

    Move Google analytics to HTML head

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
 import { serveDir } from "@std/http/file-server";
-import { useGoogleAnalytics } from "./google-analytics.ts";
 
 export default {
-  fetch: useGoogleAnalytics(Deno.env.get("GOOGLE_ANALYTICS_ID"), (request) => {
-    return serveDir(request, {
+  fetch: (request) =>
+    serveDir(request, {
       fsRoot: Deno.args[0] ?? "./static",
       showIndex: true,
-    });
-  }),
+    }),
 } satisfies Deno.ServeDefaultExport;
